da2381f89c1bdf1a83a6690c3af17c4448531388
EmanuelTejada032 <emanueltejada31295@gmail.com>
2021-07-26 15:49:15 -0400

    Add select region options, apply class only to active button

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -4,9 +4,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
-  styles: [
-  ]
+  styles: [`
+    button{
+      margin-right: 5px;
+    }
+  `]
 })
 export class ByRegionComponent{
+
+  regions: string[] = [ 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  activeRegion: string = '';
+
+  getCssClass( region: string): string {
+    return (region === this.activeRegion) ? 'btn btn-primary' : 'btn btn-outline-primary'
+  }
+
+  activateRegion(region: string){
+    this.activeRegion = region;
+  }
 
 }
